198e092003fd9fe4aad876c09461868260b38a54
MaseDar <mailspecialforspam@mail.ru>
2020-01-12 16:13:31 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-  
-  
-    
-  
 
+//print_r($_GET);
+/*INSERT INTO `challenges` (`id`, `text`, `date`) VALUES ('1', 'Отпраздновать нг!', '2019-12-31 08:00:00');*/
 
 	$db = new mysqli('localhost', 'root', '', 'todo');
 
@@ -12,6 +10,12 @@
 		exit();
 	} 
 	$db->set_charset('utf8');
+
+	/*$query = $mysqli->query('SELECT * FROM zadachi2');*/
+
+	/*while ( $row = mysqli_fetch_assoc($query) ) {
+		echo $row['text']." ".$row['date']."<br>";
+	}*/
 
 	if (isset($_POST['submit'])) {
 		$task = $_POST['task'];
@@ -22,22 +26,18 @@
 		
 	}
 	
-<<<<<<< HEAD
 
-=======
->>>>>>> fe7aeeee4f281d480c4b3dd012e36dcf3a945071
 	if (isset($_GET['delete'])) {
-   $delete = $_GET['delete'];
-   $query = "DELETE FROM tasks WHERE id = '$delete' ";
-   $run = mysqli_query($db, $query);
-    if (!$run) {
-    echo "alert('delete query failed')";
-     }
-  }
+                    $delete = $_GET['delete'];
+                    $query = "DELETE FROM tasks WHERE id = '$delete' ";
+                    $run = mysqli_query($db, $query);
+
+                    if (!$run) {
+                      echo "alert('delete query failed')";
+                    }
+                  }
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
@@ -276,11 +276,10 @@
       </div>
 
     </div>
-    <?php session_start(); ?>
     <div class="column">
       <div class="tile is-parent ">
           <article class="tile is-child notification is-dark">
-            <p class="title">Ваш список </p>
+            <p class="title">Ваш список</p>
             <p class="subtitle"></p>
             <div class="content">
               
@@ -328,6 +327,20 @@
                  
                  
                   <?php } 
+                  
+
+                  
+
+                  /*if (isset($_GET['edit'])) {
+                    $edit = $_GET['edit'];
+                    $query = "UPDATE `tasks` SET `task` = 'LOLLOLO' WHERE `tasks`.`id` = $edit ";
+                    $run2 = mysqli_query($db, $query);
+
+                    if (!$run2) {
+                      echo "alert('edit query failed')";
+                    }
+                  }*/
+
 
                   $db->close();
                   ?>

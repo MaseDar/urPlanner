27c4d79b6e98a006801a036aaef340c225c72938
dionissiy <zx053a@yandex.ru>
2020-01-28 00:36:43 +0300

    redisign log and reg 0 36

--- server/reg_case.php
+++ server/reg_case.php
@@ -42,18 +42,61 @@
       </div>
 			
       <div class="navbar-menu">
-		  
+		  <div class="navbar-menu">
+					<div class="navbar-start">
+						
+							
+					</div>
+			
+				
+					<div class="navbar-end">
+						
+						
+					       	<div class="navbar-item has-dropdown is-hoverable">
+					          
+						       <a class="navbar-link is-arrowless has-text-grey-light">
+						          v0.2 The Golden Age
+						        </a>
+						        <div class="navbar-dropdown is-boxed">
+							        <a class="navbar-item" href="https://urplanner.masedar.repl.co/">
+								        v0.0.1
+							        </a>
+							        <a class="navbar-item" href="/version_romeo/index.php">
+								        v0.1 Romeo
+							        </a> 
+							        <a class="navbar-item" href="/version_romeo/index.php">
+								        v0.1.1 Balenciaga
+							        </a> 
+							    </div>
+			      			</div>
+			      			<div class="navbar-item has-dropdown is-hoverable">
+				      			<a class="navbar-link is-arrowless has-text-grey-light">
+						         	Developers 
+						        </a>
+						        <div class="navbar-dropdown is-boxed">
+						        	<a class="navbar-item"  href="https://vk.com/masedar">MaseDar</a>
+						        	<a class="navbar-item" href="https://vk.com/mujjum">Mujjum</a>
+						        </div>
+								
+							</div>
+							<!-- Стоит dropdown из-за расстояния блоков. Если убрать has-dropdown, то между developers и profile будет расстояние -->
+						<div class="navbar-item has-dropdown is-hoverable">
+							
+						</div>
+						<div class="navbar-item">
+							<a class="button is-link is-rounded is-small" href="log_case.php">Sign in</a>
+						</div>
+					</div>
 		</nav>
 <!-- ----------------------------------------окно регистрации------------------------------------------------------ -->
 		<div class="columns">
-
-
-      
-	  
 	    	<div class="column is-4">
 		        
 	        </div>
+
 	        <div class="column is-4">
+	        	<p class="subtitle">‌‌‍‍‌‌‍‍  &#160;</p>
+	        	<p class="subtitle">‌‌‍‍‌‌‍‍  &#160;</p>
 	        	<?php if(!empty($_SESSION['error'])) : ?>
 					<div class="tile ">
 						<article class="tile  notification is-danger">
@@ -64,77 +107,68 @@
 				
 
 			<!-- <div class="has-background-dark"> -->
-			<div class="tile is-parent is-grouped is-grouped-centered">
-			  <div class="tile is-child notification is-dark is-bold">
-				<form method="post" action="" name="frmRegistration" autocomplete="off">
-					<div class="field">
-					  	<label class="label has-text-light">Username</label>
+			<div class="tile is-parent is-grouped ">
+			  	<div class="box">
+					<form method="post" action="" name="frmRegistration" autocomplete="off">
+					  	<div class="field">
+					  		<p class="title">Регистрация</p>
 						  	<div class="control has-icons-left has-icons-right">
 							    <input  name="username" class="input" type="text" placeholder="Никнейм">
-							    <span class="icon is-small is-left">
-							      <i class="fas fa-user"></i>
-							    </span>
-							    <span class="icon is-small is-right">
-							      <i class="fas fa-check"></i>
-							    </span>
-							  </div>
-							  <p class="help is-success">This username is available</p>
+							    	<span class="icon is-small is-left">
+							     		<i class="fas fa-user"></i>
+							    	</span>
+							    	<span class="icon is-small is-right has-text-success">
+							      		<i class="fas fa-check "></i>
+							    	</span>
+							 </div>
 						</div>
-
-					<div class="field">
-					  <label class="label has-text-light">Email</label>
-					  <div class="control has-icons-left has-icons-right">
-					    <input     name="email" class="input" type="email" placeholder="Email: example@yandex.ru">
-					    <span class="icon is-small is-left">
-					      <i class="fas fa-envelope"></i>
-					    </span>
-					    <span class="icon is-small is-right ">
-					      <i class="fas fa-exclamation-triangle"></i>
-					    </span>
-					  </div>
-					  <!-- <p class="help is-danger">This email is invalid</p> -->
-					</div>
-						<!-- <div class="field is-grouped"> -->
-						<div class="field">
-						<label class="label has-text-light">Password</label>
-						  <p class="control has-icons-left">
-						    <input  name="password" class="input" type="password" placeholder="Password">
-						    <span class="icon is-small is-left">
-						      <i class="fas fa-lock"></i>
-						    </span>
-						  </p>
-						</div>
+								  
+							
 
 						<div class="field">
-						<label class="label has-text-light">Password confirm</label>
-						  <p class="control has-icons-left">
-						    <input  name="passconf" class="input" type="password" placeholder="Password confirm">
-						    <span class="icon is-small is-left">
-						      <i class="fas fa-lock"></i>
-						    </span>
-						  </p>
+						  	<div class="control has-icons-left has-icons-right">
+						    	<input name="email" class="input" type="email" placeholder="Email: example@yandex.ru">
+						    		<span class="icon is-small is-left">
+						     	 		<i class="fas fa-envelope"></i>
+						    		</span>
+						    		<span class="icon is-small is-right ">
+						    	  		<i class="fas fa-exclamation-triangle"></i>
+						   			</span>
+							</div>
+						</div>
+							
+						<div class="field is-grouped is-mobile">
+							<div class="">
+							    <input  name="password" class="input" type="password" placeholder="Password">
+							    	
+							</div>
+
+							<div class="">
+							    <input  name="passconf" class="input" type="password" placeholder="Password confirm">
+							    	
+							</div>
 						</div>
 
-					<div class="field is-grouped is-grouped-centered">
-					  <div class="control ">
-					  <button name="register-user" type="submit" class="button is-primary ">Регнуться!</button>
-					  </div>
-					</div>
+						<div class="field is-grouped is-grouped-centered">
+						  	<div class="control ">
+						  		<button name="register-user" type="submit" class="button is-light">Регнуться!</button>
+						  	</div>
+						</div>
+					
 
-			<div class="field is-grouped is-grouped-centered">
-					  <div class="control">
-					  	<style type="text/css"></style>
-					  	<p class="subtitle is-6"><a class="has-text-primary" href="log_case.php">Зарегистрированы?</a></p>
-					  </div>
-					</div>
-				</div>
 
-			</form>
+						<div class="field is-grouped is-grouped-centered">
+						  	<div class="control">
+						  		<style type="text/css"></style>
+						  			<p class="subtitle is-6"><a class="has-text-primary" href="log_case.php">Зарегистрированы?</a></p>
+						  	</div>
+						</div>
+					</form>	
+				</div>		
+			</div>
 		</div>
 	</div>
 
-    
-  </div>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/js/animation.js"></script>

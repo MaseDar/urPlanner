54b58761eccc59f7d0a3bc51af8cfda22168ab0c
MaseDar PC <molotkov_vlad@mail.ru>
2020-02-10 23:41:43 +0300

    ebaniy png

---
+++ js/log.js
@@ -0,0 +1,17 @@
+//Открытие календаря, только 1 клик
+$('#notReg').click(function(){
+ 
+ 
+})
+
+  // КАРОЧЕ, ШЛЯПА ГГ!
+  
+ $('.test1').animate({ "left": "+=200%" }, 4000);
+ $('.test2').animate({ "left": "+=90%" }, 4000);
+
+  // $('.img_den').animate({opacity:0},1000);
+// $('#notReg').click(function(){
+// 	alert("HAHAHA!");
+// })
+
+ // 
